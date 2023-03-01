10142079bfdd686e841088f479dac409e894f0e7
Mayank Kumar <mayank.kumar@vlinkinfo.com>
2023-03-01 15:34:14 +0530

    Adding Mini Quiz App

--- src/components/SideNav.jsx
+++
@@ -1,9 +0,0 @@
-export const SideNav = ({results}) =>{
-
-    return <div className="side-nav">
-        <img src="" alt="" />
-        <p>Review Answers Here</p>
-        {review?.map((ans1,id)=><h4>#{id+1}:{ans1}</h4>)}
-        
-    </div>
-}
